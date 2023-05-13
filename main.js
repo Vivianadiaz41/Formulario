85ac61511f3369ef2e78856c5c5019d020a31818
viviana diaz <vivianadiazweb@gmail.com>
2023-05-13 19:57:34 -0300

    nueva modificacion

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function validarPassword(password) {
   const decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 
   if (password.value.match(decimal)) {
-    alert("El password es seguro !");
+    alert("La contraseña es segura !");
   } else {
     alert(
       "La  contraseña  debe contener  una minuscula, mayuscula, numero y un caracter especial. Y 8 carácteres como mínimo."
